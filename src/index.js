9bb582c1b00db79b867bb749f107eb14379f13a4
Massimo <rossonero64@hotmail.com>
2021-02-12 10:54:09 +0100

    update route2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import TodoContainer from "./funcBased/components/TodoContainer"
-import { HashRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 
 import "./funcBased/App.css"
 
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <TodoContainer />
         </Router>
     </React.StrictMode>, 
